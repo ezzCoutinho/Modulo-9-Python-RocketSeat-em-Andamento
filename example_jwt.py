0e3bd110104a7d259f75f778c6b628f4b00361bf
ezzcoutinho <lammerlopes@hotmail.com>
2025-04-03 20:38:56 -0300

    feat(auth): add email to JWT payload and implement token validation
    Include the user's email in the JWT payload for identification purposes. Add token validation in the `/secret` endpoint to ensure only valid tokens are processed. Enable debug mode for easier development.

--- example_jwt.py
+++ example_jwt.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 
 import jwt
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,7 @@ def login():
     token = jwt.encode(
         payload={
             "exp": datetime.now(timezone.utc) + timedelta(minutes=1),
+            "email": "rafa@email.com",
         },
         key="minhaChave",
         algorithm="HS256",
@@ -20,8 +21,17 @@ def login():
 
 @app.route("/secret", methods=["POST"])
 def secret():
+    raw_token = request.headers.get("Authorization")
+    token = raw_token.split()[1]
+
+    try:
+        token_information = jwt.decode(token, key="minhaChave", algorithms=["HS256"])
+        print(token_information)
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 400
+
     return jsonify({"message": "my secret"}), 200
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=3000, debug=True)
