cb3aedbaaed7c65872609bc9f9de5b779b956145
ezzcoutinho <lammerlopes@hotmail.com>
2025-04-07 19:58:09 -0300

    test(user_repository): update test_registry_user to test get_user_by_username

--- src/models/repositories/user_repository_test.py
+++ src/models/repositories/user_repository_test.py
@@ -2,12 +2,14 @@ from src.models.repositories.user_repository import UserRepository
 from src.models.settings.db_connection_handler import db_connection_handler
 
 
-def test_registry_user():
+def test_repository():
     db_connection_handler.connect()
     conn = db_connection_handler.get_connection()
     repo = UserRepository(conn)
 
-    username = "barney"
+    username = "Bob Esponja"
     password = "123456"
 
-    repo.registry_user(username, password)
+    user2 = repo.get_user_by_username(username)
+    print()
+    print(user2)
