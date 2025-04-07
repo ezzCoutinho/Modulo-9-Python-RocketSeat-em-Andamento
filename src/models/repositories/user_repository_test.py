4e2f5768b5d8768a84e6a7bb4d7d01c74c3f2caa
ezzcoutinho <lammerlopes@hotmail.com>
2025-04-07 20:32:42 -0300

    test(user_repository): add skipped tests for user balance editing and retrieval by username

--- src/models/repositories/user_repository_test.py
+++ src/models/repositories/user_repository_test.py
@@ -1,5 +1,7 @@
 from unittest.mock import Mock
 
+import pytest
+
 from src.models.repositories.user_repository import UserRepository
 
 
@@ -15,7 +17,8 @@ class MockConnection:
         self.commit = Mock()
 
 
-def test_repository():
+@pytest.mark.skip(reason="Test OK!")
+def test_registry_user():
     username = "Fred"
     password = "password"
 
@@ -32,3 +35,40 @@ def test_repository():
     assert "(username, password, balance)" in cursor.execute.call_args[0][0]
     assert "VALUES" in cursor.execute.call_args[0][0]
     assert cursor.execute.call_args[0][1] == (username, password, 0)
+
+
+@pytest.mark.skip(reason="Test OK!")
+def test_edit_balance():
+    user_id = 1
+    new_balance = 100.0
+
+    mock_connection = MockConnection()
+    repo = UserRepository(mock_connection)
+
+    repo.edit_balance(user_id, new_balance)
+
+    cursor = mock_connection.cursor.return_value
+    assert "UPDATE users" in cursor.execute.call_args[0][0]
+    assert "SET balance = ?" in cursor.execute.call_args[0][0]
+    assert "WHERE id = ?" in cursor.execute.call_args[0][0]
+    assert cursor.execute.call_args[0][1] == (new_balance, user_id)
+
+    mock_connection.commit.assert_called_once()
+
+
+@pytest.mark.skip(reason="Test OK!")
+def test_get_user_by_username():
+    username = "Fred"
+
+    mock_connection = MockConnection()
+    repo = UserRepository(mock_connection)
+
+    repo.get_user_by_username(username)
+
+    cursor = mock_connection.cursor.return_value
+    assert "SELECT id, username, password" in cursor.execute.call_args[0][0]
+    assert "FROM users" in cursor.execute.call_args[0][0]
+    assert "WHERE username = ?" in cursor.execute.call_args[0][0]
+    assert cursor.execute.call_args[0][1] == (username,)
+
+    cursor.fetchone.assert_called_once()
