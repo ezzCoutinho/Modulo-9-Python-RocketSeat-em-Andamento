893919760037813d901774b4a99fd8e85ff744cc
ezzcoutinho <lammerlopes@hotmail.com>
2025-04-07 20:18:07 -0300

    test(user_repository): enhance tests with mock connection for user registration

--- src/models/repositories/user_repository_test.py
+++ src/models/repositories/user_repository_test.py
@@ -1,15 +1,34 @@
+from unittest.mock import Mock
+
 from src.models.repositories.user_repository import UserRepository
-from src.models.settings.db_connection_handler import db_connection_handler
+
+
+class MockCursor:
+    def __init__(self):
+        self.execute = Mock()
+        self.fetchone = Mock()
+
+
+class MockConnection:
+    def __init__(self):
+        self.cursor = Mock(return_value=MockCursor())
+        self.commit = Mock()
 
 
 def test_repository():
-    db_connection_handler.connect()
-    conn = db_connection_handler.get_connection()
-    repo = UserRepository(conn)
+    username = "Fred"
+    password = "password"
 
-    username = "Bob Esponja"
-    password = "123456"
+    mock_connection = MockConnection()
+    repo = UserRepository(mock_connection)
 
-    user2 = repo.get_user_by_username(username)
-    print()
-    print(user2)
+    repo.registry_user(username, password)
+
+    cursor = mock_connection.cursor.return_value
+    print(cursor)
+    print(cursor.execute.call_args[0])
+
+    assert "INSERT INTO users" in cursor.execute.call_args[0][0]
+    assert "(username, password, balance)" in cursor.execute.call_args[0][0]
+    assert "VALUES" in cursor.execute.call_args[0][0]
+    assert cursor.execute.call_args[0][1] == (username, password, 0)
