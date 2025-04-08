4af8bc593a69558405f729dd0fb9dc63a56c9711
ezzcoutinho <lammerlopes@hotmail.com>
2025-04-08 20:04:22 -0300

    feat(jwt_handler): implement JWT token creation and decoding methods

---
+++ src/drivers/jwt_handler.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta, timezone
+from typing import Dict
+
+import jwt
+
+
+class JwtHandler:
+    def create_jwt_token(self, body: Dict = {}) -> str:
+        token = jwt.encode(
+            payload={
+                "exp": datetime.now(timezone.utc) + timedelta(minutes=1),
+                **body,
+            },
+            key="minhaChave",
+            algorithm="HS256",
+        )
+        return token
+
+    def decode_jwt_token(self, token: str) -> Dict:
+        token_information = jwt.decode(
+            token,
+            key="minhaChave",
+            algorithms=["HS256"],
+        )
+        return token_information
