e705cc081a6bd3736336e6f61d6b73eabaa6e218
ezzcoutinho <lammerlopes@hotmail.com>
2025-04-14 21:27:58 -0300

    feat(login): add login creator view to handle user authentication
    This commit introduces the `LoginCreatorView` class, which is responsible for handling HTTP requests for user login. It validates the input username and password and delegates the creation of the login session to the `LoginCreatorInterfaceController`. This addition is crucial for implementing the user authentication flow in the application.

---
+++ src/views/login_creator_view.py
@@ -0,0 +1,28 @@
+from src.controllers.interfaces.login_creator_interface_controller import (
+    LoginCreatorInterfaceController,
+)
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+from src.views.interfaces.view_interface import ViewInterface
+
+
+class LoginCreatorView(ViewInterface):
+    def __init__(self, controller: LoginCreatorInterfaceController) -> None:
+        self.__controller = controller
+
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        username = http_request.body.get("username")
+        password = http_request.body.get("password")
+
+        self.__validate_inputs(username, password)
+        response = self.__controller.create(username, password)
+        return HttpResponse(status_code=200, body=response)
+
+    def __validate_inputs(self, username: any, password: any) -> None:
+        if (
+            not username
+            or not password
+            or not isinstance(username, str)
+            or not isinstance(password, str)
+        ):
+            raise Exception("Invalid Input")
