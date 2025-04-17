6404c752e481b1046a738e1014a50f8f80cdcea2
ezzcoutinho <lammerlopes@hotmail.com>
2025-04-16 21:10:59 -0300

    feat(bank_routes): integrar middleware de autenticação JWT na edição de saldo
    Este commit atualiza a rota de edição de saldo para incluir a verificação do token JWT através do middleware `auth_jwt_verify`. A adição dessa funcionalidade é essencial para garantir que apenas usuários autenticados possam realizar alterações no saldo, aumentando a segurança da aplicação.

--- src/main/routes/bank_account_routes.py
+++ src/main/routes/bank_account_routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, jsonify, request
 from src.composer.balance_editor_composer import balance_editor_composer
 from src.composer.login_creator_composer import login_creator_composer
 from src.composer.user_register_composer import user_register_composer
+from src.main.middlewares.auth_jwt import auth_jwt_verify
 from src.views.http_types.http_request import HttpRequest
 
 bank_routes_bp = Blueprint("bank_routes", __name__)
@@ -24,6 +25,11 @@ def login():
 
 @bank_routes_bp.route("/bank/balance/<user_id>", methods=["PATCH"])
 def balance_editor(user_id: str):
-    http_request = HttpRequest(body=request.json or {}, params={"user_id": user_id})
+    token_information = auth_jwt_verify()
+    http_request = HttpRequest(
+        body=request.json or {},
+        params={"user_id": user_id},
+        token_info=token_information,
+    )
     http_response = balance_editor_composer().handle(http_request)
     return jsonify(http_response.body), http_response.status_code
