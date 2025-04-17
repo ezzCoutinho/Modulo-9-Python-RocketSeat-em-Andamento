7b31937eccbc81e7c24a69f6a66a140e0bd891a8
ezzcoutinho <lammerlopes@hotmail.com>
2025-04-16 21:11:04 -0300

    feat(balance_editor_view): adicionar validação de usuário no método de edição de saldo
    Este commit atualiza o método `__validate_inputs` da classe `BalanceEditorView` para incluir a validação do ID do usuário a partir dos cabeçalhos da requisição. Agora, a função verifica se o `user_id` do cabeçalho corresponde ao `user_id` fornecido na requisição, aumentando a segurança e a integridade dos dados durante a edição de saldo.

--- src/views/balance_editor_view.py
+++ src/views/balance_editor_view.py
@@ -13,11 +13,19 @@ class BalanceEditorView(ViewInterface):
     def handle(self, http_request: HttpRequest) -> HttpResponse:
         new_balance = http_request.body.get("new_balance")
         user_id = http_request.params.get("user_id")
+        headers_user_id = http_request.headers.get("uid")
 
-        self.__validate_inputs(user_id, new_balance)
+        self.__validate_inputs(user_id, new_balance, headers_user_id)
         response = self.__controller.edit(user_id, new_balance)
         return HttpResponse(status_code=200, body=response)
 
-    def __validate_inputs(self, user_id: any, new_balance: any) -> None:
-        if not user_id or not new_balance or not isinstance(new_balance, float):
+    def __validate_inputs(
+        self, user_id: any, new_balance: any, headers_user_id: any
+    ) -> None:
+        if (
+            not user_id
+            or not new_balance
+            or not isinstance(new_balance, float)
+            or int(headers_user_id) != int(user_id)
+        ):
             raise Exception("Invalid Input")
